16c0ab078c9c1c0fd7834162ce40659f8c48e7a4
Samir-kumal <kumalsameer124@gmail.com>
2026-07-11 15:43:43 +0545

    feat(notifications): add tauri-plugin-notification + capability

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -44,6 +44,7 @@ pub fn run() {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_global_shortcut::Builder::new()
                 .with_handler(|app, _shortcut, event| {
